3dfb0db8df3a713640d8243c3bde4fa81a8f4a1f
danmuth <daniel.muthukrishna@gmail.com>
2018-01-30 23:55:52 +0000

    Added comments/docstrings.

--- lensed_quasar.py
+++ lensed_quasar.py
@@ -5,7 +5,9 @@ from chainconsumer import ChainConsumer
 
 
 def get_image_positions():
-    # QSO image positions, from WFI2033_F814W.
+    """ Input image positions here. May rewrite to read positions directly from the sextractor file. """
+
+    # Image positions taken from GAIA+SExtractor for WFI2033_F814W.fits
     x_img = np.array([27.1051, 69.3759, 71.0171, 56.8204])
     y_img = np.array([33.6946, 28.1791, 58.9579, 61.2290])
 
@@ -13,6 +15,7 @@ def get_image_positions():
 
 
 def align_coords(xin, yin, pars, revert=False):
+    """ Change input coordinates (xin, yin) to be relative to the source (xmap, ymap) and then realign using theta. """
     xsource, ysource, theta, b, q = pars
 
     if revert is False:
@@ -30,6 +33,7 @@ def align_coords(xin, yin, pars, revert=False):
 
 
 def deflections(xin, yin, pars):
+    """ Calculate deflection using a mass model. Returns the calculated source positions. """
     xsource, ysource = pars[0], pars[1]
 
     x, y = align_coords(xin, yin, pars, revert=False)
@@ -40,6 +44,7 @@ def deflections(xin, yin, pars):
 
 
 def sie_model(x, y, pars):
+    """ Singular Isothermal Elliptical Mass model for lens. Returns the calculated source positions. """
     xsource, ysource, theta, b, q = pars
 
     r = np.sqrt(x ** 2 + y ** 2)
@@ -55,6 +60,7 @@ def sie_model(x, y, pars):
 
 
 def pred_positions(x_img, y_img, pars):
+    """ Predict the source positions by calculating the deflection. Returns the difference bete"""
     x, y = x_img.copy(), y_img.copy()
     x0, y0 = x_img.copy(), y_img.copy()
 
@@ -66,6 +72,7 @@ def pred_positions(x_img, y_img, pars):
 
 
 def lnlike(pars):
+    """ Calculate log-likelihood probability. Minimise the variance in the source position from all images. """
     x_img, y_img = get_image_positions()
     x_src, y_src = pred_positions(x_img, y_img, pars)
 
@@ -73,6 +80,7 @@ def lnlike(pars):
 
 
 def lnprior(pars):
+    """ Log-prior for the parameters. """
     xsource, ysource, theta, b, q = pars
     if (-np.pi < theta < np.pi) and (0. < q < 1):
         return 0.0
@@ -80,6 +88,7 @@ def lnprior(pars):
 
 
 def lnprob(pars):
+    """ Log-probability is the log-prior + log-likelihood. """
     lp = lnprior(pars)
 
     if not np.isfinite(lp):
@@ -87,9 +96,10 @@ def lnprob(pars):
     return lp + lnlike(pars)
 
 
+# MCMC setup
 ndim, nwalkers = 5, 2000
 
-# p0 = [100*np.random.rand(ndim) for i in range(nwalkers)]
+# Starting positions for the MCMC walkers sampled from a uniform distribution
 initial = OrderedDict()
 initial[r'$x_{source}$'] = np.random.uniform(low=50, high=60, size=nwalkers)
 initial[r'$y_{source}$'] = np.random.uniform(low=35, high=45, size=nwalkers)
@@ -98,25 +108,26 @@ initial[r'$b$'] = np.random.uniform(low=10, high=40, size=nwalkers)
 initial[r'$q$'] = np.random.uniform(low=0.2, high=1., size=nwalkers)
 p0 = np.transpose(list(initial.values()))
 
+# Run MCMC sampler with emcee
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
 pos, prob, state = sampler.run_mcmc(p0, 2000)
 
+# Print chain
 samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
 print(sampler.chain[:, 1, 0])
 samples = sampler.flatchain
 print(samples.shape)
 
+# Get best fit parameters
 samples_exp = samples.copy()
 samples_exp[:, 2] = np.exp(samples_exp[:, 2])
-bestfits = list(map(lambda v: (v[1]), zip(*np.percentile(samples_exp, [16, 50, 84], axis=0))))
-truth = dict(zip(initial.keys(), bestfits))
+best_fits = list(map(lambda v: (v[1]), zip(*np.percentile(samples_exp, [16, 50, 84], axis=0))))
+truth = dict(zip(initial.keys(), best_fits))
 
-centerPoint = (55.9040, 39.9600)
-
+# Plot parameter contours and mcmc chains
 c = ChainConsumer()
 c.add_chain(samples, parameters=list(initial.keys()))
 c.configure(summary=True, cloud=True)
 c.plotter.plot(filename='Figures/parameter_contours.png', truth=truth)
 fig = c.plotter.plot_walks(truth=truth, convolve=100)
 fig.savefig('Figures/mcmc_walks.png')
-
