376c002310a6e3707824566d2a7e3542f432f6aa
danmuth <daniel.muthukrishna@gmail.com>
2018-02-01 14:41:25 +0000

    Restructured file hierarchy

--- src/mass_model.py
+++ src/mass_model.py
@@ -1,15 +1,8 @@
 import numpy as np
-import emcee
-from collections import OrderedDict
-from chainconsumer import ChainConsumer
 
 
-def get_image_positions():
+def get_image_positions(x_img, y_img):
     """ Input image positions here. May rewrite to read positions directly from the sextractor file. """
-
-    # Image positions taken from GAIA+SExtractor for WFI2033_F814W.fits
-    x_img = np.array([27.1051, 69.3759, 71.0171, 56.8204])
-    y_img = np.array([33.6946, 28.1791, 58.9579, 61.2290])
 
     return x_img, y_img
 
@@ -60,7 +53,7 @@ def sie_model(x, y, pars):
 
 
 def pred_positions(x_img, y_img, pars):
-    """ Predict the source positions by calculating the deflection. Returns the difference bete"""
+    """ Predict the source positions by calculating the deflection. Returns the diff between img pos and deflections"""
     x, y = x_img.copy(), y_img.copy()
     x0, y0 = x_img.copy(), y_img.copy()
 
@@ -71,9 +64,9 @@ def pred_positions(x_img, y_img, pars):
     return x0, y0
 
 
-def lnlike(pars):
+def lnlike(pars, x_img, y_img):
     """ Calculate log-likelihood probability. Minimise the variance in the source position from all images. """
-    x_img, y_img = get_image_positions()
+    x_img, y_img = get_image_positions(x_img, y_img)
     x_src, y_src = pred_positions(x_img, y_img, pars)
 
     return -0.5 * (x_src.var() + y_src.var())
@@ -87,47 +80,10 @@ def lnprior(pars):
     return -np.inf
 
 
-def lnprob(pars):
+def lnprob(pars, x_img, y_img):
     """ Log-probability is the log-prior + log-likelihood. """
     lp = lnprior(pars)
 
     if not np.isfinite(lp):
         return -np.inf
-    return lp + lnlike(pars)
-
-
-# MCMC setup
-ndim, nwalkers = 5, 2000
-
-# Starting positions for the MCMC walkers sampled from a uniform distribution
-initial = OrderedDict()
-initial[r'$x_{source}$'] = np.random.uniform(low=50, high=60, size=nwalkers)
-initial[r'$y_{source}$'] = np.random.uniform(low=35, high=45, size=nwalkers)
-initial[r'$\theta$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-initial[r'$b$'] = np.random.uniform(low=10, high=40, size=nwalkers)
-initial[r'$q$'] = np.random.uniform(low=0.2, high=1., size=nwalkers)
-p0 = np.transpose(list(initial.values()))
-
-# Run MCMC sampler with emcee
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
-pos, prob, state = sampler.run_mcmc(p0, 2000)
-
-# Print chain
-samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
-print(sampler.chain[:, 1, 0])
-samples = sampler.flatchain
-print(samples.shape)
-
-# Get best fit parameters
-samples_exp = samples.copy()
-samples_exp[:, 2] = np.exp(samples_exp[:, 2])
-best_fits = list(map(lambda v: (v[1]), zip(*np.percentile(samples_exp, [16, 50, 84], axis=0))))
-truth = dict(zip(initial.keys(), best_fits))
-
-# Plot parameter contours and mcmc chains
-c = ChainConsumer()
-c.add_chain(samples, parameters=list(initial.keys()))
-c.configure(summary=True, cloud=True)
-c.plotter.plot(filename='Figures/parameter_contours.png', truth=truth)
-fig = c.plotter.plot_walks(truth=truth, convolve=100)
-fig.savefig('Figures/mcmc_walks.png')
+    return lp + lnlike(pars, x_img, y_img)
