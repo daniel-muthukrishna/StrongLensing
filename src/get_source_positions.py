5193f1770416eab0e75ceb7d326669c7eccb1688
danmuth <daniel.muthukrishna@gmail.com>
2018-02-09 01:03:50 +0000

    Fixed misunderstanind of xsource. Now fitting a 5 parameter model with xlens, ylens, theta, b, q. Optimising for the summed joint log-likelihoods of each image family. Correcting for redshift by calculating the angular distances and scaling the einstein radius by a factor of d_LS/d_S

--- src/get_source_positions.py
+++ src/get_source_positions.py
@@ -5,10 +5,10 @@ from collections import OrderedDict
 from chainconsumer import ChainConsumer
 from src.mass_model import lnprob
 import matplotlib.pyplot as plt
-from image_overplot_contours import plot_image
+from src.image_overplot_contours import plot_image
 
 
-def run_mcmc(x_img, y_img, fig_dir, ndim=5, nwalkers=100, steps=200, prior_func=None, initial_params=None, fits_file=None, img_name=''):
+def run_mcmc(x_img, y_img, fig_dir, zl=None, zs=None, ndim=5, nwalkers=100, steps=200, prior_func=None, initial_params=None, fits_file=None, img_name=''):
 
     # Starting positions for the MCMC walkers sampled from a uniform distribution
     if initial_params is None:
@@ -23,7 +23,7 @@ def run_mcmc(x_img, y_img, fig_dir, ndim=5, nwalkers=100, steps=200, prior_func=
     p0 = np.transpose(list(initial.values()))
 
     # Run MCMC sampler with emcee
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x_img, y_img, prior_func))
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x_img, y_img, zl, zs, prior_func))
     pos, prob, state = sampler.run_mcmc(p0, steps)
 
     # Print chain
@@ -84,13 +84,13 @@ def lensed_quasar():
 
 def macs0451():
     fig_dir = 'Figures/MACS0451/'
-    nwalkers = 1000
-    steps = 2000
+    nwalkers = 1500
+    steps = 3000
 
-    # # ImageA Positions
-    # img_name = '_ImageA'
-    # x_img = np.array([2375.942110, 2378.5, 2379.816610, 2381.299088, 2384, 2385.927991, 2389.555816, 2457.694760, 2450.744242, 2442.833333, 2437.857924, 2433.064587, 2427.166666, 2424.099866, 2418.5, 2416.444081, 2462])
-    # y_img = np.array([3038.016677, 3024, 3012.367933, 2999.365293, 2983.5, 2970.435199, 2955.945319, 2737.545077, 2752.305849, 2766.166666, 2782.058508, 2795.293450, 2811.166666, 2823.079067, 2837.5, 2846.943113, 2728])
+    # ImageA Positions
+    img_name = '_ImageA'
+    x_img = np.array([2375.942110, 2378.5, 2379.816610, 2381.299088, 2384, 2385.927991, 2389.555816, 2457.694760, 2450.744242, 2442.833333, 2437.857924, 2433.064587, 2427.166666, 2424.099866, 2418.5, 2416.444081, 2462])
+    y_img = np.array([3038.016677, 3024, 3012.367933, 2999.365293, 2983.5, 2970.435199, 2955.945319, 2737.545077, 2752.305849, 2766.166666, 2782.058508, 2795.293450, 2811.166666, 2823.079067, 2837.5, 2846.943113, 2728])
 
     # # Image 1.1 Positions
     # img_name = '_Image1.1'
@@ -107,24 +107,24 @@ def macs0451():
     # x_img = np.array([3222.796159, 3227.700108])
     # y_img = np.array([3550.903781, 3542.180780])
 
-    # Image 3.1 Positions
-    img_name = '_Image3.1'
-    x_img = np.array([2933.063074, 3393.715824])
-    y_img = np.array([2943.400421, 3398.196336])
+    # # Image 3.1 Positions
+    # img_name = '_Image3.1'
+    # x_img = np.array([2933.063074, 3393.715824])
+    # y_img = np.array([2943.400421, 3398.196336])
 
 
 
     # Starting positions for the MCMC walkers sampled from a uniform distribution
     initial = OrderedDict()
-    initial[r'$x_{source}$'] = np.random.uniform(low=2400, high=3400, size=nwalkers)
-    initial[r'$y_{source}$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
+    initial[r'$x_{lens}$'] = np.random.uniform(low=2400, high=4000, size=nwalkers)
+    initial[r'$y_{lens}$'] = np.random.uniform(low=2000, high=3700, size=nwalkers)
     initial[r'$\theta$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-    initial[r'$b$'] = np.random.uniform(low=10, high=1500, size=nwalkers)
+    initial[r'$b$'] = np.random.uniform(low=10, high=2000, size=nwalkers)
     initial[r'$q$'] = np.random.uniform(low=0.2, high=1., size=nwalkers)
 
     def lnprior(pars):
         xsource, ysource, theta, b, q = pars
-        if (2400 < xsource < 3300) and (2400 < ysource < 3400) and (10 < b < 1500) and (-np.pi < theta < np.pi) and (0. < q < 1):
+        if (2400 < xsource < 4000) and (2000 < ysource < 3700) and (10 < b < 2000) and (-np.pi < theta < np.pi) and (0. < q < 1):
             return 0.0
         else:
             return -np.inf
@@ -136,67 +136,50 @@ def macs0451():
 
 def macs0451_multiple_sources():
     fig_dir = 'Figures/MACS0451/'
-    ndim, nwalkers = 17, 1000
-    steps = 2000
+    ndim, nwalkers = 5, 1500
+    steps = 3000
     img_name = '_multiple_sources'
+    zl = 0.43
 
-    x_img, y_img = {}, {}
+    x_img, y_img, zs = {}, {}, {}
     x_img['A'] = np.array([2375.942110, 2378.5, 2379.816610, 2381.299088, 2384, 2385.927991, 2389.555816, 2457.694760, 2450.744242, 2442.833333, 2437.857924, 2433.064587, 2427.166666, 2424.099866, 2418.5, 2416.444081, 2462])
     y_img['A'] = np.array([3038.016677, 3024, 3012.367933, 2999.365293, 2983.5, 2970.435199, 2955.945319, 2737.545077, 2752.305849, 2766.166666, 2782.058508, 2795.293450, 2811.166666, 2823.079067, 2837.5, 2846.943113, 2728])
+    zs['A'] = 2.01
 
     x_img['11'] = np.array([3557.178601, 3548.271886, 3541.407488])
     y_img['11'] = np.array([3363.943860, 3375.285957, 3385.515024])
+    zs['11'] = 2.06
 
-    x_img['62'] = np.array([3486.371962])
-    y_img['62'] = np.array([3069.305065])
+    # x_img['62'] = np.array([3486.371962])
+    # y_img['62'] = np.array([3069.305065])
+    # zl['62'] = 1.405
 
     x_img['41'] = np.array([3222.796159, 3227.700108])
     y_img['41'] = np.array([3550.903781, 3542.180780])
+    zs['41'] = 1.810
 
     x_img['31'] = np.array([2933.063074, 3393.715824])
     y_img['31'] = np.array([2943.400421, 3398.196336])
+    zs['31'] = 1.904
 
     # Starting positions for the MCMC walkers sampled from a uniform distribution
     initial = OrderedDict()
-    initial[r'${x_{source}}_A$'] = np.random.uniform(low=2700, high=4000, size=nwalkers)
-    initial[r'${y_{source}}_A$'] = np.random.uniform(low=2000, high=3700, size=nwalkers)
-    initial[r'$\theta_A$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-
-    initial[r'${x_{source}}_11$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'${y_{source}}_11$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'$\theta_11$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-
-    initial[r'${x_{source}}_62$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'${y_{source}}_62$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'$\theta_62$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-
-    initial[r'${x_{source}}_41$'] = np.random.uniform(low=2400, high=3400, size=nwalkers)
-    initial[r'${y_{source}}_41$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'$\theta_41$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-
-    initial[r'${x_{source}}_31$'] = np.random.uniform(low=2400, high=3400, size=nwalkers)
-    initial[r'${y_{source}}_31$'] = np.random.uniform(low=2400, high=3300, size=nwalkers)
-    initial[r'$\theta_31$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
-
-    initial[r'$b$'] = np.random.uniform(low=10, high=1500, size=nwalkers)
+    initial[r'$x_{lens}$'] = np.random.uniform(low=2400, high=4000, size=nwalkers)
+    initial[r'$y_{lens}$'] = np.random.uniform(low=2000, high=3700, size=nwalkers)
+    initial[r'$\theta$'] = np.random.uniform(low=-np.pi, high=np.pi, size=nwalkers)
+    initial[r'$b$'] = np.random.uniform(low=10, high=2000, size=nwalkers)
     initial[r'$q$'] = np.random.uniform(low=0.2, high=1., size=nwalkers)
 
     def lnprior(pars):
-        xsource_A, ysource_A, theta_A, xsource_11, ysource_11, theta_11, xsource_62, ysource_62, theta_62, \
-        xsource_41, ysource_41, theta_41, xsource_31, ysource_31, theta_31, b, q = pars
-        if (2700 < xsource_A < 4000) and (2000 < ysource_A < 3700)  and (-np.pi < theta_A < np.pi)\
-                and (2700 < xsource_11 < 4000)and (2400 < ysource_11 < 3300) and (-np.pi < theta_11 < np.pi) \
-                and (2700 < xsource_62 < 4000) and (2400 < ysource_62 < 3300) and (-np.pi < theta_62 < np.pi) \
-                and (2700 < xsource_41 < 4000) and (2400 < ysource_41 < 3400) and (-np.pi < theta_41 < np.pi) \
-                and (2700 < xsource_31 < 4000) and (2400 < ysource_31 < 3400) and (-np.pi < theta_31 < np.pi) \
-                and (10 < b < 1500) and (0. < q < 1):
+        xlens, ylens, theta, b, q = pars
+        if (2400 < xlens < 4000) and (2000 < ylens < 3700) and (-np.pi < theta < np.pi) and (10 < b < 2000) and (0. < q < 1):
             return 0.0
         else:
             return -np.inf
 
     fits_file = '/Users/danmuth/PycharmProjects/StrongLensing/data/MACS0451/MACS0451_F110W.fits'
 
-    run_mcmc(x_img, y_img, fig_dir, ndim=ndim, nwalkers=nwalkers, steps=steps, prior_func=lnprior, initial_params=initial, fits_file=fits_file, img_name=img_name)
+    run_mcmc(x_img, y_img, fig_dir, zl, zs, ndim=ndim, nwalkers=nwalkers, steps=steps, prior_func=lnprior, initial_params=initial, fits_file=fits_file, img_name=img_name)
 
 
 if __name__ == '__main__':
--- src/mass_model.py
+++ src/mass_model.py
@@ -1,41 +1,57 @@
 import numpy as np
+from astropy.cosmology import LambdaCDM
 
+
+def scale_einstein_radius(b, zl, zs, H0=70, Om0=0.3, Ode0=0.7):
+    if zl is None or zs is None:
+        return b
+
+    cosmo = LambdaCDM(H0=H0, Om0=Om0, Ode0=Ode0)
+    d_ls = cosmo.angular_diameter_distance_z1z2(zl, zs)
+    d_s = cosmo.angular_diameter_distance_z1z2(0, zs)
+    d_l = cosmo.angular_diameter_distance_z1z2(0, zl)
+
+    d = d_ls / d_s
+
+    return b / d.value
 
 
 def align_coords(x_in, y_in, pars, revert=False):
-    """ Change input coordinates (xin, yin) to be relative to the source (xmap, ymap) and then realign using theta. """
-    xsource, ysource, theta, b, q = pars
+    """ Change input coordinates (xin, yin) to be relative to the lens (xmap, ymap) and then realign using theta. """
+    xlens, ylens, theta, b, q = pars
 
     if revert is False:
-        xmap = x_in - xsource  # xmap is the image position wrt to Xsource at (0,0) in image plane
-        ymap = y_in - ysource
-        x = xmap * np.cos(theta) + ymap * np.sin(theta)  # Rotates coordinates from image plane to source plane
+        xmap = x_in - xlens  # xmap is the image position wrt to Xlens at (0,0) in image plane
+        ymap = y_in - ylens
+        x = xmap * np.cos(theta) + ymap * np.sin(theta)  # Rotates coordinates from image plane to lens plane
         y = ymap * np.cos(theta) - xmap * np.sin(theta)
     else:
-        xmap = x_in * np.cos(theta) - y_in * np.sin(theta)  # Rotates coordinates from source plane to image plane (Xsource is still at Origin)
+        xmap = x_in * np.cos(theta) - y_in * np.sin(theta)  # Rotates coordinates from lens plane to image plane (Xlens is still at Origin)
         ymap = y_in * np.cos(theta) + x_in * np.sin(theta)
-        x = xmap + xsource  # x is the deflection position but
-        y = ymap + ysource
+        x = xmap + xlens  # x is the deflection position but
+        y = ymap + ylens
 
     return x, y
 
 
-def deflections(x_img, y_img, pars):
+def deflections(x_img, y_img, zl, zs, pars):
     """ Calculate deflection using a mass model. Returns the calculated source positions. """
-    xsource, ysource = pars[0], pars[1]
+    xlens, ylens = pars[0], pars[1]
 
-    x, y = align_coords(x_img, y_img, pars, revert=False)  # Image positions rotated to source plane coordinates (wrt Xsource at (0,0))
-    xout, yout = sie_model(x, y, pars)  # Calculated deflection positions in source plane coordinates
-    x, y = align_coords(xout, yout, pars, revert=True)  # Calculated deflection positions in image plane coordinates (wrt Xsource at (0,0))
+    x, y = align_coords(x_img, y_img, pars, revert=False)  # Image positions rotated to lens plane coordinates (wrt Xlens at (0,0))
+    xout, yout = sie_model(x, y, zl, zs, pars)  # Calculated deflection positions in lens plane coordinates
+    x, y = align_coords(xout, yout, pars, revert=True)  # Calculated deflection positions in image plane coordinates (wrt Xlens at (0,0))
 
-    x_deflect, y_deflect = x - xsource, y - ysource  # Calculated deflection positions in image plane coordinates (actual pixel positions instead of wrt Xsource)
+    x_deflect, y_deflect = x - xlens, y - ylens  # Calculated deflection positions in image plane coordinates (actual pixel positions instead of wrt Xlens)
 
     return x_deflect, y_deflect
 
 
-def sie_model(x, y, pars):
+def sie_model(x, y, zl, zs, pars):
     """ Singular Isothermal Elliptical Mass model for lens. Returns the calculated source positions. """
-    xsource, ysource, theta, b, q = pars
+    xlens, ylens, theta, b, q = pars
+
+    b = scale_einstein_radius(b=b, zl=zl, zs=zs)
 
     r = np.sqrt(x ** 2 + y ** 2)
     if q == 1.:
@@ -49,42 +65,32 @@ def sie_model(x, y, pars):
     return xout, yout
 
 
-def pred_positions(x_img, y_img, pars):
+def pred_positions(x_img, y_img, zl, zs, pars):
     """ Predict the source positions by calculating the deflection. Returns the diff between img pos and deflections"""
     x, y = x_img.copy(), y_img.copy()
     x0, y0 = x_img.copy(), y_img.copy()
 
-    x_deflect, y_deflect = deflections(x, y, pars)  # Calculated deflection positions in image plane coordinates (pixel positions)
+    x_deflect, y_deflect = deflections(x, y, zl, zs, pars)  # Calculated deflection positions in image plane coordinates (pixel positions)
     x_src = x0 - x_deflect  # Calculated source positions in pixels
     y_src = y0 - y_deflect
-
 
     return x_src, y_src
 
 
-def lnlike(pars, x_img, y_img):
+def lnlike(pars, x_img, y_img, zl, zs):
     """ Calculate log-likelihood probability. Minimise the variance in the source position from all images. """
     if isinstance(x_img, dict):
         x_src, y_src = {}, {}
-        pars_dict = {}
         lnlike_dict = {}
 
-        xsource_A, ysource_A, theta_A, xsource_11, ysource_11, theta_11, xsource_62, ysource_62, theta_62, \
-        xsource_41, ysource_41, theta_41, xsource_31, ysource_31, theta_31, b, q = pars
-        pars_dict['A'] = xsource_A, ysource_A, theta_A, b, q
-        pars_dict['11'] = xsource_11, ysource_11, theta_11, b, q
-        pars_dict['62'] = xsource_62, ysource_62, theta_62, b, q
-        pars_dict['41'] = xsource_41, ysource_41, theta_41, b, q
-        pars_dict['31'] = xsource_31, ysource_31, theta_A, b, q
-
         for key in x_img:
-            x_src[key], y_src[key] = pred_positions(x_img[key], y_img[key], pars_dict[key])
+            x_src[key], y_src[key] = pred_positions(x_img[key], y_img[key], zl, zs[key], pars)
             lnlike_dict[key] = -0.5 * (x_src[key].var() + y_src[key].var())
 
         return sum(lnlike_dict.values())
 
     else:
-        x_src, y_src = pred_positions(x_img, y_img, pars)
+        x_src, y_src = pred_positions(x_img, y_img, zl, zs, pars)
 
         return -0.5 * (x_src.var() + y_src.var())
 
@@ -93,7 +99,7 @@ def lnprior(pars, prior_func=None):
     """ Log-prior for the parameters. """
 
     if prior_func is None:
-        xsource, ysource, theta, b, q = pars
+        xlens, ylens, theta, b, q = pars
 
         if (-np.pi < theta < np.pi) and (0. < q < 1):
             return 0.0
@@ -103,18 +109,18 @@ def lnprior(pars, prior_func=None):
         return prior_func(pars)
 
 
-def lnprob(pars, x_img, y_img, prior_func=None):
+def lnprob(pars, x_img, y_img, zl, zs, prior_func=None):
     """ Log-probability is the log-prior + log-likelihood. """
     lp = lnprior(pars, prior_func)
 
     if not np.isfinite(lp):
         return -np.inf
-    return lp + lnlike(pars, x_img, y_img)
+    return lp + lnlike(pars, x_img, y_img, zl, zs)
 
 
 # if __name__ == '__main__':
-#     xsource, ysource, theta, b, q = (53, 40.8, 1.55, 24.1, 0.954)
-#     pars = (xsource, ysource, theta, b, q)
+#     xlens, ylens, theta, b, q = (53, 40.8, 1.55, 24.1, 0.954)
+#     pars = (xlens, ylens, theta, b, q)
 #     x_img, y_img = np.array([69.3759]), np.array([28.1791])
 #     x_src, y_src = pred_positions(x_img, y_img, pars)
 #
