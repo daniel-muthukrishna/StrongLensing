ced738249284a186828bc222bd83421207fc8dce
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2018-03-04 15:48:59 +0000

    Plot best fit image positions

--- lensing/get_img_pos.py
+++ lensing/get_img_pos.py
@@ -17,24 +17,25 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.join(SCRIPT_DIR, '..')
 
 
-def get_quasar_img_pos():
+def plot_img_pos(x1=3034., y1=3053., s1=1.3, lx=3034., ly=3053., lb=950., lq=0.8, lp=110.):
+    fig_dir = 'Figures/MACS0451/'
+    sa = (2000, 5000)  # search area is 2000 pixels to 5000 pixels
 
     # Define source positions as a Guassian surface brightness profile
-    X1 = pymc.Uniform('X1', 50., 60., value=54.83)
-    Y1 = pymc.Uniform('Y1', 35., 45., value=38.98)
-    R1 = pymc.Uniform('R1', 0., 20., value=3.95)
-    Q1 = pymc.Uniform('Q1', 0.2, 1., value=0.8)
-    P1 = pymc.Uniform('P1', -180., 180., value=54.)
-    S1 = pymc.Uniform('N1', 0.6, 6., value=1.3)
+    X1 = pymc.Uniform('X1', 2500., 3900., value=x1)
+    Y1 = pymc.Uniform('Y1', 2400., 3500., value=y1)
+    Q1 = pymc.Uniform('Q1', 0.2, 1., value=1.)
+    P1 = pymc.Uniform('P1', -180., 180., value=0.)
+    S1 = pymc.Uniform('N1', 0.6, 6., value=s1)
     src = SBObjects.Gauss('', {'x':X1,'y':Y1,'q':Q1,'pa':P1,'sigma':S1})
     srcs = [src]
 
     # Define lens mass model
-    LX = pymc.Uniform('lx', 50., 60., value=54.83)
-    LY = pymc.Uniform('ly', 35., 45., value=38.98)
-    LB = pymc.Uniform('lb', 10., 40., value=20.)
-    LQ = pymc.Uniform('lq', 0.2, 1., value=0.8)
-    LP = pymc.Uniform('lp', -180., 180., value=110.)
+    LX = pymc.Uniform('lx', 2900., 3400., value=lx)
+    LY = pymc.Uniform('ly', 2600., 3500., value=ly)
+    LB = pymc.Uniform('lb', 10., 1500., value=lb)
+    LQ = pymc.Uniform('lq', 0.2, 1., value=lq)
+    LP = pymc.Uniform('lp', -180., 180., value=lp)
     XB = pymc.Uniform('xb', -0.2, 0.2, value=0.)
     XP = pymc.Uniform('xp', -180., 180., value=0.)
     lens = MassModels.SIE('', {'x': LX, 'y': LY, 'b': LB, 'q': LQ, 'pa': LP})
@@ -44,19 +45,18 @@ def get_quasar_img_pos():
     # x_lens, y_lens, theta, b, q = (51.5, 39.9, 1.1, 21.6, 1.0)
     # pars = (x_lens, y_lens, theta, b, q)
 
-    x, y = np.meshgrid(np.arange(0, 100, 1.), np.arange(0, 100, 1.))
+    x, y = np.meshgrid(np.arange(sa[0], sa[1], 1.), np.arange(sa[0], sa[1], 1.))
 
-    for lens in lenses:
-        lens.setPars()
-    x_src, y_src = pylens.getDeflections(lenses, [x, y])
+    x_src, y_src = pylens.getDeflections(lenses, [x, y], )
     # x_src, y_src = pred_positions(x, y, d=1, pars=pars)
 
     image_plane = src.pixeval(x_src, y_src)
+
+    plt.figure()
     plt.imshow(image_plane, interpolation='nearest', origin='lower')
+    plt.savefig(os.path.join(ROOT_DIR, fig_dir, 'image_plane.png'))
 
-    print(pylens.getImgPos(x0=0, y0=0, b=21.6, sx=54.83, sy=38.98, lenses=lenses))
-
-    plt.show()
+    # print(pylens.getImgPos(x0=0, y0=0, b=21.6, sx=54.83, sy=38.98, lenses=lenses))
 
 
 def get_macs0451_img_pos():
@@ -66,9 +66,9 @@ def get_macs0451_img_pos():
     # Define source positions as a Guassian surface brightness profile
     X1 = pymc.Uniform('X1', 2500., 3900., value=3034)
     Y1 = pymc.Uniform('Y1', 2400., 3500., value=3053)
-    Q1 = pymc.Uniform('Q1', 1., 1., value=1.)
-    P1 = pymc.Uniform('P1', 0., 0., value=0)
-    S1 = pymc.Uniform('N1', 0.6, 6., value=0.7)
+    Q1 = pymc.Uniform('Q1', 0.2, 1., value=1.)
+    P1 = pymc.Uniform('P1', -180., 180., value=0.)
+    S1 = pymc.Uniform('N1', 0.6, 6., value=1.3)
     src = SBObjects.Gauss('', {'x': X1, 'y': Y1, 'q': Q1,'pa': P1, 'sigma': S1})
     srcs = [src]
     pars = [X1, Y1, S1]  # List of parameters
@@ -94,7 +94,7 @@ def get_macs0451_img_pos():
 
     # MCMC setup
     nwalkers = 100
-    nsteps = 1000
+    nsteps = 2000
     z_lens = 0.43
 
     # Observed Image positions
@@ -170,6 +170,8 @@ def get_macs0451_img_pos():
     samples_exp = samples.copy()
     samples_exp[:, 2] = np.exp(samples_exp[:, 2])
     best_fits = list(map(lambda v: (v[1]), zip(*np.percentile(samples_exp, [16, 50, 84], axis=0))))
+    print('bestfits')
+    print(best_fits)
 
     # Plot parameter contours and mcmc chains
     param_names = ['$x_{src}$', '$y_{src}$', '$\sigma_{src}$']
@@ -181,6 +183,9 @@ def get_macs0451_img_pos():
     fig = c.plotter.plot_walks(convolve=100)
     fig.savefig(os.path.join(ROOT_DIR, fig_dir, 'source_pos_mcmc_walks.png'))
 
+    b = best_fits
+    plot_img_pos(x1=b[0], y1=b[1], s1=b[2], lx=b[3], ly=b[4], lb=b[5], lq=b[6], lp=b[7])
+
     # print(image_coords_pred)
     # plt.show()
     # return image_coords_pred
@@ -191,6 +196,6 @@ def get_macs0451_img_pos():
 
 
 if __name__=='__main__':
-    # get_quasar_img_pos()
     get_macs0451_img_pos()
+    # plot_img_pos(x1=3034., y1=3053., s1=1.3, lx=3034., ly=3053., lb=950., lq=0.8, lp=110.)
     plt.show()
