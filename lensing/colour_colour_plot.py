38ba2d34f8f2abeafb93141f098c378e8e49e4bc
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2018-04-24 18:40:51 +0100

    Selecting on colour magnitude diagram

--- lensing/colour_colour_plot.py
+++ lensing/colour_colour_plot.py
@@ -25,7 +25,7 @@ def read_sextractor_output(filepath):
     return sex
 
 
-def get_positions(filepath, colour1=None, colour2=None, colour_limits=None, fits_file=None, fig_dir='.'):
+def get_positions(filepath, colour1=None, colour2=None, colour_limits=None, fits_file=None, fig_dir='.', addsavename=''):
     sex = read_sextractor_output(filepath)
     x = sex.X_IMAGE
     y = sex.Y_IMAGE
@@ -35,30 +35,38 @@ def get_positions(filepath, colour1=None, colour2=None, colour_limits=None, fits
         plot_image(fits_file, vmin=0, vmax=0.35)
     plt.scatter(x, y, marker='o', c='b', alpha=0.2)
 
-    if colour1 is not None:
-        (x1, y1), (x2, y2) = colour_limits
+    if colour_limits is not None:
+        (x1, x2), (y1, y2) = colour_limits
         mask = (colour1 > x1) & (colour1 < x2) & (colour2 > y1) & (colour2 < y2)
         x = x[mask]
         y = y[mask]
 
-    plt.scatter(x, y, marker='+', c='g', alpha=0.8)
+    plt.scatter(x, y, marker='+', c='g', alpha=0.9)
 
     plt.xlim(min(x), max(x))
     plt.ylim(min(x), max(y))
-    plt.savefig(os.path.join(fig_dir, 'cluster_members'))
+    plt.savefig(os.path.join(fig_dir, 'cluster_members{}'.format(addsavename)))
     return x, y
 
 
 def get_colour(filepath1, filepath2):
-    sex1 = read_sextractor_output(filepath1)
-    sex2 = read_sextractor_output(filepath2)
+    # If one of the args is None return the other flux instead of colour
+    if filepath1 is None:
+        flux = read_sextractor_output(filepath2).FLUX_ISO
+        return flux
+    elif filepath2 is None:
+        flux = read_sextractor_output(filepath1).FLUX_ISO
+        return flux
 
-    colour = sex1.FLUX_ISO / sex2.FLUX_ISO
+    flux1 = read_sextractor_output(filepath1).FLUX_ISO
+    flux2 = read_sextractor_output(filepath2).FLUX_ISO
+
+    colour = flux1 / flux2
 
     return colour
 
 
-def plot_colour_colour(colour1, colour2, name1='', name2='', fig_dir='.', colour_limits=None, fpathpos=None):
+def plot_colour_colour(colour1, colour2, name1='', name2='', fig_dir='.', colour_limits=None, addsavename='', fpathpos=None, axlims=None):
     fig, ax = plt.subplots(1)
     ax.scatter(colour1, colour2, marker='.', alpha=0.3)
 
@@ -74,29 +82,33 @@ def plot_colour_colour(colour1, colour2, name1='', name2='', fig_dir='.', colour
                 arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
 
     if colour_limits is not None:
-        (x1, y1), (x2, y2) = colour_limits
+        (x1, x2), (y1, y2) = colour_limits
         rect = patches.Rectangle(xy=(x1, y1), width=x2-x1, height=y2-y1, fill=False)
         ax.add_patch(rect)
 
     ax.set(xlabel=name1, ylabel=name2)
-    ax.set_xlim(-0.1, 2)
-    ax.set_ylim(-0.1, 2)
-    fig.savefig(os.path.join(fig_dir, 'colour_colour_plot.png'))
+    if axlims:
+        (x1, x2), (y1, y2) = axlims
+        ax.set_xlim(x1, x2)
+        ax.set_ylim(y1, y2)
+    fig.savefig(os.path.join(fig_dir, 'colour_colour_plot{}'.format(addsavename)))
 
 
 def main():
     fig_dir = os.path.join(ROOT_DIR, 'Figures')
+    fpath140 = os.path.join(ROOT_DIR, 'data/MACS0451/sex_140.asc')
     fpath110 = os.path.join(ROOT_DIR, 'data/MACS0451/sex_110.asc')
     fpath814 = os.path.join(ROOT_DIR, 'data/MACS0451/sex_814.asc')
     fpath606 = os.path.join(ROOT_DIR, 'data/MACS0451/sex_606.asc')
 
-    colour1 = get_colour(fpath606, fpath814)
-    colour2 = get_colour(fpath814, fpath110)
+    colour1 = get_colour(fpath814, None)
+    colour2 = get_colour(fpath606, fpath814)
 
-    colour_limits = [(0.525, 0.2), (0.73, 0.33)]
+    colour_limits = [(50, 750), (0.5, 0.72)]
+    axlims = [(-10, 800), (-0.1, 2)]
 
-    plot_colour_colour(colour1, colour2, name1='606A - 814A', name2='814A - 1100A', fig_dir=fig_dir, colour_limits=colour_limits)#, fpathpos=fpath110)
-    get_positions(fpath110, colour1, colour2, colour_limits=colour_limits, fits_file=os.path.join(ROOT_DIR, 'data/MACS0451/MACS0451_F110W.fits'))
+    plot_colour_colour(colour1, colour2, name1='814A', name2='606A - 814A', fig_dir=fig_dir, colour_limits=colour_limits, addsavename='_814', axlims=axlims)  #, fpathpos=fpath110)
+    get_positions(fpath110, colour1, colour2, colour_limits=colour_limits, fits_file=os.path.join(ROOT_DIR, 'data/MACS0451/MACS0451_F110W.fits'), fig_dir=fig_dir, addsavename='_814')
     plt.show()
 
 
